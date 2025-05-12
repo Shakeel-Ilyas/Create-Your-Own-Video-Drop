c60b582a82db0fec8d6b40444f98861c72db0dbe
Shakeel Ilyas <mshakeel117db@gmail.com>
2025-05-12 18:12:04 +0500

    Uploading my project on github.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+function openVideo(evt, video_img) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("video_content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(video_img).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function openForm() {
+    const videoOpen = document.getElementById("form_section");
+    videoOpen.style.display = "block";
+}
